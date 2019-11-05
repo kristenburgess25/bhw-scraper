dcd444e97a035b6b507e77b35e2cd54e7c0af3a7
Kristen Burgess <kristenburgess@Kristens-MacBook.local>
2019-11-05 15:54:57 -0700

    succesfully scraping title, date, location, link- need to clean up location

--- bhw-scraper.js
+++ bhw-scraper.js
@@ -17,9 +17,31 @@
           const Info = rowData.text()
           const eventTitle = $(rowData).find('a').text()
           const detailLink = $(rowData).find('a').attr('href')
+          const eventDate = $(rowData).first().text()
+          const eventLocation = $(rowData).last().text()
+     
+        
+        //   const detailUrl = `https://barrelhorseworld.com/${detailLink}`
+
+        const detailUrl =  'https://barrelhorseworld.com/eventdetail.asp?ID=126020'
+
+          axios(detailUrl)
+            .then(response => {
+                const html = response.data;
+                const $ = cheerio.load(html)
+                const detailsTable = $('tbody > tr');
+                const rightColText = $(detailsTable).find('.text-right').text()
+                // if(rightColText == 'When') {
+                //     let sibling 
+                //     console.log
+                // }
+                // console.log('TEXT RIGHT', rightCol)
+            })
 
           events.push({
             title: eventTitle,
+            date: eventDate,
+            location: eventLocation,
             link: detailLink
           });
         });
