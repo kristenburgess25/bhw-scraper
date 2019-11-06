bfc244e47d5ecf075cfe9b4162b911840a3adfeb
Kristen Burgess <kristenburgess@Kristens-MacBook.local>
2019-11-06 15:09:02 -0700

    successfully merging base data with some details on single event

--- bhw-scraper.js
+++ bhw-scraper.js
@@ -3,6 +3,7 @@
 
     const url = 'https://barrelhorseworld.com/events.asp';
     const events = []
+    const fullData = []
 
     axios(url)
       .then(response => {
@@ -14,11 +15,11 @@
         eventsTable.each(function () {
         
           const rowData = $(this).find('td');
-        //   const Info = rowData.text()
-        //   const eventTitle = $(rowData).find('a').text()
+          const Info = rowData.text()
+          const eventTitle = $(rowData).find('a').text()
           const detailLink = $(rowData).find('a').attr('href')
-        //   const eventDate = $(rowData).first().text()
-        //   const eventLocation = $(rowData).last().text()
+          const eventDate = $(rowData).first().text()
+          const eventLocation = $(rowData).last().text()
         //   const eventDetails = ''
           
      
@@ -26,55 +27,97 @@
           const detailUrl = `https://barrelhorseworld.com/${detailLink}`
 
 
-          axios(detailUrl)
-            .then(response => {
-                const html = response.data;
-                const $ = cheerio.load(html)
-                const detailsTable = $('tbody > tr')
+          events.push({
+            title: eventTitle,
+            date: eventDate,
+            location: eventLocation,
+            link: detailUrl
+          });
 
-                detailsTable.each(function() {
-                    const event = {
-                        name: '',
-                        date: '',
-                        location: '',
-                        description: '',
-                        contact: '',
-                        phone: ''
-                    }
-
-                    const rightColText = $(this).find('.text-right').text()
-                    const siblings = $(this).find('.text-right').siblings().text()
-                    // console.log('descriptor', rightColText, 'SIBLINGS', siblings)
-
-
-                    if(rightColText === 'Description') {
-                        // console.log('right col sibling', siblings)
-                        event.description = siblings
-                    }
-
-                    if(rightColText === 'Contact') {
-                        // console.log('right col sibling', siblings)
-                        event.contact = siblings
-                    }
-
-                    
-                    
-                    // console.log('DETAIL OBJ', eventDetails)
-                    // console.log('DETAILS', event)
-                    events.push(event)
-                    
-                })
-                
-            })
-
-        //   events.push({
-        //     title: eventTitle,
-        //     date: eventDate,
-        //     location: eventLocation,
-        //     link: detailUrl
-        //   });
+        //   console.log(events)
         });
-
-        
+        getDetails(events[5])
       })
       .catch(console.error);
+    
+    // function getDetails(event) {
+    //     const details = {
+    //         description: '',
+    //         contact: '',
+    //     }
+
+    //     axios(event.link)
+    //     .then(response => {
+    //         const html = response.data 
+    //         const $ = cheerio.load(html)
+    //         const detailsTable = $('tbody')
+
+    //         const rightColText = $(detailsTable).find('.text-right').text()
+    //         const siblings = $(detailsTable).find('.text-right').siblings().text()
+
+    //         if(rightColText === 'Description') {
+    //             console.log('right col sibling', siblings)
+    //             details.description = siblings
+    //         }
+
+    //         if(rightColText === 'Contact') {
+    //             console.log('right col sibling', siblings)
+    //             details.contact = siblings
+    //         }
+    //     })
+    // }
+
+
+
+    // fetching the details 
+
+    function getDetails(event) {
+        // console.log('the event', event)
+        axios(event.link)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html)
+            const detailsTable = $('tbody > tr')
+            const details = {
+                description: '',
+                contact: '',
+            }
+  
+            detailsTable.each(function() {
+                const baseEvent = event 
+                // console.log('BASE EVENT CHECK', baseEvent)
+
+  
+                const rightColText = $(this).find('.text-right').text()
+                const siblings = $(this).find('.text-right').siblings().text()
+                // console.log('descriptor', rightColText, 'SIBLINGS', siblings)
+  
+  
+                if(rightColText === 'Description') {
+                    console.log('right col sibling', siblings)
+                    details.description = siblings
+                }
+  
+                if(rightColText === 'Contact') {
+                    console.log('right col sibling', siblings)
+                    details.contact = siblings
+                }
+                // console.log('all events', events)
+            })
+            console.log('THE EVENT', event)
+            console.log('details', details)
+            const mergedData = {...event, ...details}
+            console.log('MERGED DATA', mergedData)
+            
+        })
+        
+        // fullData.push({
+
+        // })
+    }
+
+    function updateEvent(event) {
+        console.log('full event')
+    }
+
+
