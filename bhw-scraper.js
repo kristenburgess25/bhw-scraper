f041aec30c32b0991b0ec6bb333b4c57096bd746
Kristen Burgess <kristenburgess@Kristens-MacBook.local>
2019-11-06 15:53:17 -0700

    code cleanup

--- bhw-scraper.js
+++ bhw-scraper.js
@@ -11,7 +11,6 @@
         const $ = cheerio.load(html)
         const eventsTable = $('tbody > tr');
         
-
         eventsTable.each(function () {
         
           const rowData = $(this).find('td');
@@ -20,10 +19,7 @@
           const detailLink = $(rowData).find('a').attr('href')
           const eventDate = $(rowData).first().text()
           const eventLocation = $(rowData).last().text()
-        //   const eventDetails = ''
-          
-     
-        
+
           const detailUrl = `https://barrelhorseworld.com/${detailLink}`
 
           const event = {
@@ -33,46 +29,14 @@
             link: detailUrl
           }
           getDetails(event);
-
-        //   console.log(events)
         });
-        // getDetails(events[5])
       })
       .catch(console.error);
-    
-    // function getDetails(event) {
-    //     const details = {
-    //         description: '',
-    //         contact: '',
-    //     }
-
-    //     axios(event.link)
-    //     .then(response => {
-    //         const html = response.data 
-    //         const $ = cheerio.load(html)
-    //         const detailsTable = $('tbody')
-
-    //         const rightColText = $(detailsTable).find('.text-right').text()
-    //         const siblings = $(detailsTable).find('.text-right').siblings().text()
-
-    //         if(rightColText === 'Description') {
-    //             console.log('right col sibling', siblings)
-    //             details.description = siblings
-    //         }
-
-    //         if(rightColText === 'Contact') {
-    //             console.log('right col sibling', siblings)
-    //             details.contact = siblings
-    //         }
-    //     })
-    // }
-
 
 
     // fetching the details 
 
     function getDetails(event) {
-        // console.log('the event', event)
         axios(event.link)
         .then(response => {
             const html = response.data;
@@ -85,41 +49,21 @@
   
             detailsTable.each(function() {
                 const baseEvent = event 
-                // console.log('BASE EVENT CHECK', baseEvent)
-
   
                 const rightColText = $(this).find('.text-right').text()
                 const siblings = $(this).find('.text-right').siblings().text()
-                // console.log('descriptor', rightColText, 'SIBLINGS', siblings)
   
   
                 if(rightColText === 'Description') {
-                    // console.log('right col sibling', siblings)
                     details.description = siblings
                 }
   
                 if(rightColText === 'Contact') {
-                    // console.log('right col sibling', siblings)
                     details.contact = siblings
                 }
-                // console.log('all events', events)
             })
-            // console.log('THE EVENT', event)
-            // console.log('details', details)
             const mergedData = {...event, ...details}
-            // console.log('MERGED DATA', mergedData)
             events.push(mergedData)
         })
-        // .catch(console.error);
-
-        // fullData.push({
-
-        // })
+        .catch(console.log('hit the catch'));
     }
-
-
-    // function updateEvent() {
-    //     console.log('ALL EVENTS', events)
-    // }
-
-
